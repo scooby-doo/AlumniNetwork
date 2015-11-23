f8720c59d80739d12a4d6686a15dbb915b214cac
Scooby <tanya.moldovan@gmail.com>
2015-11-23 10:38:23 +0200

    small fix

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -3,8 +3,8 @@ class Startup < ActiveRecord::Base
   belongs_to :user
   delegate :first_name, :last_name, :user_name, :email, :to => :user, :prefix => :true
   #paginates_per 4
-  has_many :comments
-  has_many :upvotes
+  has_many :comments, dependent: :destroy
+  has_many :upvotes, dependent: :destroy
 
   acts_as_taggable
   acts_as_taggable_on :fields
